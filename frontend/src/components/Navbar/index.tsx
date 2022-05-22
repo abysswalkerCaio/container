d32d49970149bad4f5da36fc13cd00c955379580
Caio Costa <hcaio370@gmail.com>
2022-05-22 16:40:57 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import './style.css';
+
+function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>Movimentação de Containers</h1>
+                    <a href="https://github.com/abysswalkerCaio" target="_blank" rel="noreferrer">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/abysswalkerCaio</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
